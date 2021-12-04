c726f9b6778025be900e61856963a5e9d19948b3
Kashish Jain <55389276+Kashishjain04@users.noreply.github.com>
2021-12-04 14:57:40 +0530

    dense grid layout

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -33,13 +33,83 @@ const Notes = () => {
 	return (
 		<>
 			{loading && <Loader />}
-			<div className="notes">
-				{notes?.map((note) => (
-					<SingleNote note={note} key={note.id} deleteNote={deleteNote} />
-				))}
-			</div>
+			<NotesSm notes={notes} deleteNote={deleteNote} />
+			<NotesMd notes={notes} deleteNote={deleteNote} />
+			<NotesLg notes={notes} deleteNote={deleteNote} />
+			<NotesXl notes={notes} deleteNote={deleteNote} />
 		</>
 	);
 };
 
 export default Notes;
+
+// 1 col
+const NotesSm = ({ notes, deleteNote }) => (
+	<div className="notes sm">
+		<div>
+			{notes?.map((note) => (
+				<SingleNote note={note} key={note.id} deleteNote={deleteNote} />
+			))}
+		</div>
+	</div>
+);
+// 2 cols
+const NotesMd = ({ notes, deleteNote }) => (
+	<div className="notes md">
+		<div>
+			{notes?.filter((_, index) => index%2 === 0)?.map((note) => (
+				<SingleNote note={note} key={note.id} deleteNote={deleteNote} />
+			))}
+		</div>
+		<div>
+			{notes?.filter((_, index) => index%2 !== 0)?.map((note) => (
+				<SingleNote note={note} key={note.id} deleteNote={deleteNote} />
+			))}
+		</div>
+	</div>
+);
+// 3 cols
+const NotesLg = ({ notes, deleteNote }) => (
+	<div className="notes lg">
+		<div>
+			{notes?.filter((_, index) => index%3 === 0)?.map((note) => (
+				<SingleNote note={note} key={note.id} deleteNote={deleteNote} />
+			))}
+		</div>
+		<div>
+			{notes?.filter((_, index) => index%3 === 1)?.map((note) => (
+				<SingleNote note={note} key={note.id} deleteNote={deleteNote} />
+			))}
+		</div>
+		<div>
+			{notes?.filter((_, index) => index%3 === 2)?.map((note) => (
+				<SingleNote note={note} key={note.id} deleteNote={deleteNote} />
+			))}
+		</div>
+	</div>
+);
+// 4 cols
+const NotesXl = ({ notes, deleteNote }) => (
+	<div className="notes xl">
+		<div>
+			{notes?.filter((_, index) => index%4 === 0)?.map((note) => (
+				<SingleNote note={note} key={note.id} deleteNote={deleteNote} />
+			))}
+		</div>
+		<div>
+			{notes?.filter((_, index) => index%4 === 1)?.map((note) => (
+				<SingleNote note={note} key={note.id} deleteNote={deleteNote} />
+			))}
+		</div>
+		<div>
+			{notes?.filter((_, index) => index%4 === 2)?.map((note) => (
+				<SingleNote note={note} key={note.id} deleteNote={deleteNote} />
+			))}
+		</div>
+		<div>
+			{notes?.filter((_, index) => index%4 === 3)?.map((note) => (
+				<SingleNote note={note} key={note.id} deleteNote={deleteNote} />
+			))}
+		</div>
+	</div>
+);
